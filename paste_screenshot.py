771a11c44ff2a6eac871a1658128657cefde0ca2
curiousdeveloper_jv <57355660+jyothiv-codes@users.noreply.github.com>
2024-02-17 12:21:13 -0800

    Takes screenshot and saves it in a folder
    Every 15 seconds, the program asks if a screenshot is required. If yes, within the next 5 seconds, it takes the screenshot, giving the user time to set the window to be captured. This loop continues forever until the user terminates the process. I got this idea when I was working with PostgreSQL commands for practice and wanted to capture the commands and their respective output in a folder that I could later push to GitHub.

---
+++ paste_screenshot.py
@@ -0,0 +1,65 @@
+import pyautogui
+import time
+import os
+import tkinter as tk
+
+
+
+
+
+def paste_screenshot():
+    # Captures screenshot, saves it to the specified folder and shows a pop-up message if the screenshot was successful
+    message=""
+    try:
+        screenshot = pyautogui.screenshot()  
+        filename = f"Screenshot_{time.strftime('%Y%m%d_%H%M%S')}.png"  # The file is created with the current timestamp
+        screenshot.save(os.path.join("/Users/jyothivaidyanathan/projects/PostgreSQL-Practice", filename))  # Save the screenshot
+        message = "Success!"
+    except:
+        message="Failure!"
+
+    # Create the main window of the pop-up
+    root = tk.Tk()
+    root.title("Screenshot message")
+    # Label widget is used to display the image
+    label = tk.Label(root, text=message)
+    label.pack()
+
+   
+
+    # Set the desired window size
+    window_width = 400
+    window_height = 200
+
+    # This helps center the message
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    offset_x = (screen_width - window_width) // 2
+    offset_y = (screen_height - window_height) // 2
+
+    geometry_string = f"{window_width}x{window_height}+{offset_x}+{offset_y}"
+
+    # Apply the geometry to the root window
+    root.geometry(geometry_string)
+
+    # Event loop is started to display the window
+    root.mainloop()
+
+
+
+
+while True:
+    print("Do you want to click a screenshot now? (y/n)")
+    time.sleep(15)
+    answer=input()
+    if answer=="y":
+        print("taking screenshot")
+        time.sleep(5)
+        paste_screenshot()
+    else:
+        pass
+
+
+
+
